f52394800855dd0b2caff817e8d2761ecc4573b6
hrishikeshgoyal27 <hrishi.goyal@gmail.com>
2014-03-26 22:31:05 +0530

    Added all App files

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var path = ApplicationData.Current.LocalFolder.Path + @"\Users.db";
+                var db = new SQLiteAsyncConnection(path);
+                await db.CreateTableAsync<User>();
+            }
+            catch (Exception)
+            { }
+
+            //Button_Click_1.visibility = Visibility.Collapsed;
+        }
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var path = ApplicationData.Current.LocalFolder.Path + @"\Users.db";
+            var db = new SQLiteAsyncConnection(path);
+            var data = new User
+            {
+                name=txt_name.Text,
+                email=txt_email.Text
+            
+            };
+            int i = await db.InsertAsync(data);
+           
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            string result = "";
+            var path = ApplicationData.Current.LocalFolder.Path + @"\Users.db";
+            var db = new SQLiteAsyncConnection(path);
+            List<User> all = await db.QueryAsync<User>("Select* from Users");
+            var count = all.Any() ? all.Count : 0;
+
+            foreach (var item in all)
+            {
+                result += "email" + item.email.ToString() + "\nFstname" + item.name+"\n";
+            }
+
+            MessageDialog msg = new MessageDialog(result.ToString());
+            await msg.ShowAsync();
+        }
+    }
+}
---
+++ App4/App4/User.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    [Table("Users")]
+    class User
+    {
+    [PrimaryKey, Unique]
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
